c46b2e39d67e225abd341c36dd0302a048fb39f3
Rsja12 <asocarras10@gmail.com>
2019-09-21 18:06:13 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+
+    attr_accessor :word 
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+       array.find_all do |element|
+        (word.split("").sort) == (element.split("").sort)
+       end
+    end
+
+    #initialize with a word.
+
+    #iterate through an array of words with .find_all 
+    #split up the word that you're checking and sort those letters.
+    #Then compare the split up letters of @word to make sure they match the split up and sorted letters of element (each element in the array of words)
+
+end
